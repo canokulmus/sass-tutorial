18234a8b03930afa556475d1c8bf85d4423f471d
CAN OKULMUS <e2400265@ceng.metu.edu.tr>
2022-11-17 01:02:03 +0300

    final update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./style/map-get.scss";
+import "./style/nesting.scss";
+import "./style/mixin.scss";
+import "./style/function.scss";
+import "./style/extend.scss";
+import "./style/if-else.scss";
 
-function App() {
+
+const App: React.FC = () => {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* for Map-get example */}
+      {/* <h5 className='ex-map-get'>Map-get example</h5> */}
+
+      {/* for nesting example */}
+      {/* <div className="card">
+        <h4 className="card-title">Lorem</h4>
+        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab labore odit accusantium. Quis nobis ad et numquam odit vel eligendi sit, rerum nostrum ea voluptate? Consectetur dicta ipsa excepturi exercitationem!</p>
+        <ul className="list">
+          <li className="list-item"><a href="#">Link 1</a></li>
+          <li className="list-item"><a href="#">Link 2</a></li>
+          <li className="list-item"><a href="#">Link 3</a></li>
+        </ul>
+      </div> */}
+
+
+      {/* mixin example */}
+      {/* <div className="card2">
+        <h4 className="card-title2">Lorem</h4>
+        <p className="card-text2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab labore odit accusantium. Quis nobis ad et numquam odit vel eligendi sit, rerum nostrum ea voluptate? Consectetur dicta ipsa excepturi exercitationem!</p>
+      </div> */}
+
+      {/* scss functions example */}
+      {/* <div className="card3">
+        <div className="first"><span>first</span></div>
+        <div className="second"><span>second</span></div>
+        <div className="third"><span>third</span></div>
+        <div className="x"><span>x</span></div>
+        <div className="y"><span>y</span></div>
+      </div> */}
+
+      {/* scss extend example */}
+      {/* <div className="error">error message</div>
+      <div className="success">success message</div>
+      <div className="warning">warning message</div> */}
+
+      {/* If-else scss example  */}
+      <div className="profile">&#9728;</div>
+      <div className="profile">&#9738;</div>
+      <div className="profile">&#9828;</div>
+
     </div>
   );
 }
